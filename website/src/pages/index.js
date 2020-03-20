4a043dd9f5dc9468ff3c0c83d7b63fe5f47a348b
Jefferson Licet <jeffersonlicet@gmail.com>
2020-03-20 03:10:44 -0300

    Updating styles

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -42,7 +42,7 @@ function Feature({
   imageUrl, title, description, icon,
 }) {
   return (
-    <div className={classnames('w-1/3 p-4 pt-8 pb-2 border border-gray-200 shadow-sm mr-2 rounded-lg flex flex-col items-center ml-2 text-center', styles.feature)}>
+    <div className={classnames('w-full lg:w-1/3 p-4 lg:pt-8 pb-2 lg:border border-gray-200 lg:shadow-sm lg:mr-2 rounded-lg flex flex-col items-center lg:ml-2 text-center', styles.feature)}>
       <div className="pb-4 primary opacity-50">
         { icon }
       </div>
@@ -60,9 +60,9 @@ function Home() {
       description="Create beautiful immersive animations using shared components"
     >
       <MotionLayoutProvider debug>
-        <header className="bg-gray-200 pt-10 min-h-screen">
+        <header className="bg-gray-200 xl:min-h-screen">
           <div className="flex w-full justify-between flex-col lg:flex-row">
-            <div className="w-full lg:w-1/2 md:ml-8 xl:ml-16 ml-0 mt-32">
+            <div className="w-full lg:w-1/2 md:ml-8 xl:ml-16 ml-0 lg:mt-20 xl:mt-32 p-10 lg:p-0">
               <div className="text-5xl baloo font-bold primary leading-tight">Motion Layout</div>
               <div className="text-4xl text-gray-500 mt-4 leading-tight">Create beautiful immersive animations using shared components.</div>
               <div className="mt-12">
@@ -71,11 +71,11 @@ function Home() {
                 </Button>
               </div>
             </div>
-            <div className="w-full lg:w-auto mt-8 px-10">
+            <div className="w-full lg:w-auto lg:px-10 mt-8">
               <HomeDemo />
             </div>
           </div>
-          <div className="mouse hidden lg:block">
+          <div className="mouse hidden xl:block">
             <div className="mouse-icon">
               <span className="mouse-wheel" />
             </div>
@@ -84,8 +84,8 @@ function Home() {
         <main>
           {features && features.length && (
             <section className={styles.features}>
-              <div className="container pt-12">
-                <div className="flex">
+              <div className="container lg:pt-12">
+                <div className="flex flex-col lg:flex-row">
                   {features.map((props, idx) => (
                     // eslint-disable-next-line react/no-array-index-key
                     <Feature key={idx} {...props} />
@@ -97,7 +97,7 @@ function Home() {
           <div className="flex justify-center py-10  flex-col items-center">
             <div className="text-5xl baloo text-white font-bold primary leading-tight">What?</div>
             <div className="mt-10 mb-20">
-              <div className="text-center text-gray-600 text-xl max-w-3xl justify-center flex px-4">
+              <div className="text-center text-gray-600 px-8 lg:p-0 lg:text-xl max-w-3xl justify-center flex px-4">
                 There are amazing libraries like framer-motion that help you create animations when mounting or unmounting components. But, if two views have the same image in different positions and sizes, they cannot be animated together. With Motion Layout, you can link components together to animate them when changing views.
               </div>
             </div>
